6b73b4ad27323cc24ab0d39ff595c02cfa672576
Fabi <fabiandreacola@gmail.com>
2015-08-11 21:54:53 +0200

    resume effect

---
+++ app/assets/javascripts/resume.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  $(".wa, .ul, .ts, .sb ").hide();
+
+  $(".wagon").click(function() {
+    $(".wa").fadeToggle('slow/400')
+
+  });
+
+  $(".ulg").click(function() {
+    $(".ul").fadeToggle('slow/400')
+
+  });
+
+  $(".tsc").click(function() {
+    $(".ts").fadeToggle('slow/400')
+
+  });
+
+  $(".sbs").click(function() {
+    $(".sb").fadeToggle('slow/400')
+
+  });
+
+
+
+
+
+});
