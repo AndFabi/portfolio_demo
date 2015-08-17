6edfce53424b1b49dba776cdc0392dca4ef4fc2d
Fabi <fabiandreacola@gmail.com>
2015-08-17 21:14:51 +0200

    anim end

--- app/assets/javascripts/scroll_anim.js
+++ app/assets/javascripts/scroll_anim.js
@@ -2,21 +2,23 @@
 $(document).ready(function() {
 
 
-  $(window).scroll(function(e){
-    if ($(this).scrollTop() > 300) {
-      $(".navbar").css("margin-top", "-70px");
-    }
-    else {
-      $(".navbar").css("margin-top", "0px");
-    }
-  });
+
+    // navbar transition jQuery script
+    $(window).scroll(function(e){
+      if ($(this).scrollTop() > 300) {
+        $(".navbar").css("margin-top", "-12px");
+      }
+      else {
+        $(".navbar").css("margin-top", "0px");
+      }
+    });
 
 
 
 
 
 
-
+    //Scrool to section
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -24,9 +26,15 @@ $(document).ready(function() {
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 800);
         return false;
       }
     }
   });
+
+  //scroll slide animation
+  window.sr = new scrollReveal();
+
+
+
 });
