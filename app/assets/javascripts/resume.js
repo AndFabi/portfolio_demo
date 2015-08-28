a10fc2c80e80e64d68de28f17d72b6207594e6db
Fabi <fabiandreacola@gmail.com>
2015-08-28 13:42:56 +0200

    jquery fix

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -2,25 +2,50 @@ $(document).ready(function() {
 
   $(".wa, .ul, .ts, .sb ").hide();
 
-  $(".wagon").hover(function() {
-    $(".wa").fadeToggle('fast')
+  $(".wagon").click(function() {
+    $(".resume").fadeToggle('slow', function(){
+      $('.resume').replaceWith('<p class="resume">Fullstack web developer, Bootcamp développeur web, prototypage d’application, développement de projets, gestion des ressources.<br> mai-juin 2015, Le wagon</p>')
+
+    });
 
   });
 
-  $(".ulg").hover(function() {
-    $(".ul").fadeToggle('fast')
+  $(".ulg").click(function() {
+    $(".resume").fadeToggle('slow', function(){
+      $('.resume').replaceWith('<p class="resume">Master en sciences politiques finalité spécialisée en relations internationales.<br>2010 - 2012, Ulg</p>')
+    });
 
   });
 
-  $(".tsc").hover(function() {
-    $(".ts").fadeToggle('fast')
-
+  $(".tsc").click(function() {
+    $(".resume").fadeToggle('slow', function(){
+      $('.resume').replaceWith('<p class="resume">The Smart Company Développement de projets, Recherche de financements publics et support de gestion de projets.<br> décembre 2013-janvier 2015, Liège</p>')
+    });
   });
 
-  $(".sbs").hover(function() {
-    $(".sb").fadeToggle('fast')
-
+  $(".sbs").click(function() {
+    $(".resume").fadeToggle('slow', function(){
+      $('.resume').replaceWith('<p class="resume">Step by Steppes Création, réalisation et suivi de projets, activité de conseil en octroi de subsides et gestion de projets collaboratifs.<br> septembre 2013, Liège</p>')
+    });
   });
+
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('#restartResume').length) {
+      $(".resume").fadeToggle('slow', function(){
+        $('.resume').replaceWith('<p class="resume">De la Science Politique au web developement<br>Une approche pluridisciplinaire est essentiel pour comprendre toutes les implications de la "troisième révolution industrielle".<br>La Science Politique me permets de comprendre le contexte d&#39; une problématique et la programmation me permet d&#39; apporter <br> une réponse technique ciblé pour la résoudre.')
+    });
+    }
+  });
+
+  // picture foter geek
+  $(".profil").hover(
+    function() {
+    $(this).attr('src', '/assets/geek_foot.png');
+    },
+    function() {
+      $(this).attr('src', '/assets/fabien.jpg');
+    }
+  );
 
 
 });
